7b7759fc25a0db602873d970f65fd6048c9d8b40
Dmitriy Kaminskiy <dimitriy.kaminskiy@nextiva.com>
2017-09-15 13:25:09 +0300

    add react-grid-layout

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import ReactGridLayout from 'react-grid-layout';
+import './App.css';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ReactGridLayout className="layout" cols={12} rowHeight={30} width={1200}>
+          <div key="a" data-grid={{x: 0, y: 0, w: 1, h: 2, static: true}}>a</div>
+          <div key="b" data-grid={{x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4}}>b</div>
+          <div key="c" data-grid={{x: 4, y: 0, w: 1, h: 2}}>c</div>
+        </ReactGridLayout>
+      </div>
+    );
+  }
+}
